6a10018bc964eb5a7eda7a93f674d21e888d98a6
muhammadfarhan-zin <muhammad.farhan@zintechnologies.com>
2024-10-31 16:57:02 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  //  make a function that updates the count and sends a message to react native app that this is the value of count
   const updateCount = (count) => {
-    //  send message to react native app
     setCount(count);
-    window.ReactNativeWebView.postMessage(count);
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(count);
+    }
     console.log('count: ', count);
-  }
+  };
 
   return (
     <>
@@ -27,14 +27,12 @@ function App() {
       </div>
       <h1>Deeplinking with React Native</h1>
       <div className="card">
-        <button onClick={() =>
-          updateCount(count + 1)
-        }>
+        <button onClick={() => updateCount(count + 1)}>
           count is {count}
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
